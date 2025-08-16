9b7f2ab81d0327df0dbe306263cbce64a8ba3882
Kavana <kavanasp218@gmail.com>
2025-08-16 16:27:02 +0530

    update

---
+++ src/test/java/pac1/TC007_Select_Dropdown.java
@@ -0,0 +1,33 @@
+package pac1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC007_Select_Dropdown {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://tutorialsninja.com/demo/index.php?");
+		
+		driver.findElement(By.linkText("Desktops")).click();
+		driver.findElement(By.linkText("Mac(1)")).click();
+		WebElement sort=driver.findElement(By.id("input-sort"));
+		Select sle=new Select(sort);
+		//sle.selectByIndex(5);
+		List<WebElement> elecount=sle.getOptions();
+		for(int i=0;i<elecount.size();i++) {
+			System.out.println("The values:"+elecount.get(i).getText());
+		}
+
+	}
+
+}
---
+++ src/test/java/pac1/TC009_Windowhandling.java
@@ -0,0 +1,34 @@
+package pac1;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TC009_Windowhandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://letcode.in/window");
+		driver.findElement(By.id("home")).click();
+			String pwindow=driver.getWindowHandle();
+			System.out.println("parent window:"+pwindow);
+			System.out.println("url is:"+driver.getCurrentUrl());
+			driver.findElement(By.id("multi")).click();
+			Set<String> mwindows=driver.getWindowHandles();
+			
+			for(String cwindow:mwindows) {
+				driver.switchTo().window(cwindow);
+				System.out.println("url is:"+driver.getCurrentUrl());
+			}
+			driver.close();//one window will close
+			driver.quit();//entire window will close
+
+	}
+
+}
