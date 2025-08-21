574d10618a63b2015f855ac72802a3bf3d98d707
Kavana <kavanasp218@gmail.com>
2025-08-21 15:23:38 +0530

    update

--- src/test/java/pac1/TC001_Selenium.java
+++ src/test/java/pac1/TC001_Selenium.java
@@ -1,5 +1,10 @@
 package pac1;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,9 +13,29 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TC001_Selenium {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
+		Properties prob=new Properties();
+		FileInputStream fis=new FileInputStream("C:\\Users\\KAVANA\\eclipse-workspace\\Wipro__javaselenium\\login.properties");
+		prob.load(fis);
+		String url=prob.getProperty("url");
+		String username=prob.getProperty("username");
+		String password=prob.getProperty("password");
+		
 		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		String title=driver.getTitle();
+		System.out.println("The Title is:"+title);
+		Thread.sleep(3000);
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+		
+		
+		/*WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		String title=driver.getTitle();
@@ -20,6 +45,6 @@ public class TC001_Selenium {
 		//username.sendKeys("Admin");
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();*/
 }
 }
